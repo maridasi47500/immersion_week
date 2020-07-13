35738a717c488121bd389f2219e5db87dff34da8
myanonymoususername-that-blows-your-face <coderinthedark@email.email>
2020-07-13 15:00:46 +0200

    exo6

---
+++ Day 0/exo 5/05_organise_again.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mkdir Immersion\ week
+cd Immersion\ week
+mkdir Day\ 0 
+mkdir Day\ 1
+mkdir Day\ 2 
+mkdir Day\ 3 
+mkdir Day\ 4 
+
+touch Day\ 0/learning.md 
+touch Day\ 1/learning.md
+touch Day\ 2/learning.md 
+touch Day\ 3/learning.md 
+touch Day\ 4/learning.md 
+cd ..
+
