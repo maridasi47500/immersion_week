8976e38bff582b854a2e68dc1dac88ae9c9738f9
anonyme_de_la_region <anonyme201504@gmail.com>
2020-07-14 23:25:03 +0200

    day3

--- README.md
+++
@@ -1,3 +0,0 @@
-hello
-mes exercices durant cette formation
-il y a le resume des acquis de chaque jour ----------> dans fichiers s'appelnt learnings.md
---
+++ web/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>exercises inco</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <p>day 3</p>
+        <p><a href="../Day 2/01_displayFullName.html">ex 1 - displayfull name</a></p>
+    </body>
+</html>
+
+
+<?php 
